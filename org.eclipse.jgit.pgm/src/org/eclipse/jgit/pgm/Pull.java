320b6499f8351dd43f80593648530acc4073693e
Ian <mahaffy.2@wright.edu>
2020-04-12 15:29:38 -0400

    Added --test option to pull; Added test methods for output;
    Requirements:
    Resolves: Req 1.2
    
    Issues:
    Fix: #11

--- org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Pull.java
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Pull.java
@@ -20,9 +20,11 @@ import java.io.IOException;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.PullCommand;
+import org.eclipse.jgit.api.PullResult;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Constants;
 import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
 
 @Command(common = true, usage = "usage_PullChangesFromRemoteRepositories")
 class Pull extends TextBuiltin {
@@ -41,8 +43,20 @@ class Pull extends TextBuiltin {
 	private String remoteBranchName = null;
 
 	/**
-	 * Run method creates new pull using specified commands. References: Req.
-	 * 1.0, Req. 1.1
+	 * Option to print out test output. References: Req. 1.2
+	 */
+	@Option(name = "--test", aliases = { "-t" })
+	private boolean showTest;
+
+	/**
+	 * Variable that holds the referenced remote string, used for testing.
+	 * References: Req. 1.2
+	 */
+	private String ref;
+
+	/**
+	 * Run method creates new pull using specified commands, and setting the
+	 * options accordingly. References: Req. 1.0, Req. 1.1, Req. 1.2
 	 */
 	@Override
 	protected void run() throws IOException {
@@ -53,10 +67,121 @@ class Pull extends TextBuiltin {
 			if (remoteBranchName != null)
 				pull.setRemoteBranchName(remoteBranchName);
 
-			pull.call();
+			PullResult result = pull.call();
+
+			try {
+				ref = pull.getRemote() + "/" + pull.getRemoteBranchName(); //$NON-NLS-1$
+			} catch (Exception e) {
+				throw die(e.getMessage(), e);
+			}
+
+			if (showTest) {
+				printTest(pull);
+				printTest(result);
+			}
 
 		} catch (GitAPIException e) {
 			throw die(e.getMessage(), e);
 		}
+	}
+
+	/**
+	 * Prints the information of the pull command. Used for testing and
+	 * troubleshooting purposes. References: Req. 1.2
+	 *
+	 * @param pull
+	 *            PullCommand to be examined and printed out
+	 * @throws IOException
+	 */
+	@SuppressWarnings("nls")
+	private void printTest(final PullCommand pull) throws IOException {
+		outw.println(
+				"\n***************** Pull Command Details ***************\n");
+
+		StringBuilder enteredOptions = new StringBuilder();
+		enteredOptions.append("****** Entered Options *****\n");
+		if (remote != null)
+			enteredOptions.append("Remote:  " + remote + ";  ");
+		else
+			enteredOptions.append("Remote:  !RETURNED NULL;  ");
+
+		if (remoteBranchName != null)
+			enteredOptions
+					.append("Remote Branch:  " + remoteBranchName + ";  ");
+		else
+			enteredOptions.append(
+					"Remote Branch:  !RETURNED NULL, default will be used;  ");
+
+		if (ref != null)
+			enteredOptions.append("Internal ref Variable:  " + ref + ";  ");
+		else
+			enteredOptions.append("Internal ref Variable:  !RETURNED NULL;  ");
+
+		enteredOptions.append("\n\n****************************\n");
+
+		outw.println(enteredOptions.toString());
+
+		if (pull.toString() != null)
+			outw.println("To String:  " + pull.toString());
+
+		if (pull.getRemote() != null)
+			outw.println("Remote:  " + pull.getRemote());
+		else
+			outw.println("Remote: NO REMOTE FOUND");
+
+		if (pull.getRemoteBranchName() != null)
+			outw.println("Remote Branch:   " + pull.getRemoteBranchName());
+		else
+			outw.println("Remote Branch:  NO REMOTE BRANCH FOUND");
+
+		outw.println(
+				"\n*****************************************************\n\n");
+	}
+
+	/**
+	 * Prints the information of the pull result. Used for testing and
+	 * troubleshooting purposes. References: Req. 1.2
+	 *
+	 * @param results
+	 *            PullResult to be examined and printed out
+	 * @throws IOException
+	 */
+	@SuppressWarnings("nls")
+	private void printTest(final PullResult results) throws IOException {
+		outw.println(
+				"\n***************** Pull Result Options ***************\n");
+
+		outw.println("Pull Results Successful:  " + results.isSuccessful());
+
+		if (results.toString() != null)
+			outw.println("ToString:  " + results.toString());
+		else
+			outw.println("ToString:   NONE FOUND");
+
+		if (results.getFetchResult() != null)
+			outw.println(
+					"Fetch Result:  " + results.getFetchResult().toString());
+		else
+			outw.println("Fetch Result:   NONE FOUND");
+
+		if (results.getFetchedFrom() != null)
+			outw.println("Fetched From:  " + results.getFetchedFrom());
+		else
+			outw.println("Fetched From:   NONE FOUND");
+
+		if (results.getMergeResult() != null)
+			outw.println(
+					"Merge Result:  " + results.getMergeResult().toString());
+		else
+			outw.println("Merge Result:   NONE FOUND");
+
+		if (results.getRebaseResult() != null)
+			outw.println(
+					"Rebase Result: " + results.getRebaseResult().toString());
+		else
+			outw.println("Rebase Result: NONE FOUND");
+
+		outw.println(
+				"\n*****************************************************\n\n");
 	}
 }
